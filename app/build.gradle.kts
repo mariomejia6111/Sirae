5ff21080cda724ae8d478cc394e4833de12f625d
mario <mario.mejia3@catolica.edu.sv>
2023-07-27 16:13:41 -0600

    3:42

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,5 +43,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.android.gms:play-services-auth:20.6.0")
+    implementation ("com.google.android.gms:play-services-auth-api-phone:18.0.1")
+
 
 }
--- app/src/main/java/com/example/sirae/Login.kt
+++ app/src/main/java/com/example/sirae/Login.kt
@@ -1,6 +1,8 @@
 package com.example.sirae
-
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.Auth
@@ -15,11 +17,63 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+        // Declarar el botón con el ID "btn_google"
+        val btnGoogle = findViewById<Button>(R.id.btn_google)
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .build()
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // Configurar el escucha de clics del botón
+        btnGoogle.setOnClickListener {
+            signInWithGoogle()
+        }
+    }
+
+    private fun signInWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+
+    private fun handleSignInResult(completedTask: com.google.android.gms.tasks.Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            // El inicio de sesión con Google fue exitoso, puedes obtener la información del usuario aquí
+
+            val email = account?.email
+            val displayName = account?.displayName
+            // Puedes obtener más información del usuario según tus necesidades
+
+            // Luego de obtener la información, puedes redirigir a la siguiente pantalla o realizar otras acciones
+            goToMainActivity()
+        } catch (e: ApiException) {
+            Log.e("LoginActivity", "Error al obtener información del usuario: ${e.message}")
+            Toast.makeText(this, "Error al obtener información del usuario", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    private fun goToMainActivity() {
+       // val intent = Intent(this, MainActivity::class.java)
+
+        // Puedes pasar datos adicionales a MainActivity si lo deseas
+        // intent.putExtra("email", email)
+
+        ///startActivity(intent)
+
+        // Finalizar esta actividad para que el usuario no pueda regresar a la pantalla de inicio de sesión con el botón "Atrás"
+        finish()
     }
 }
